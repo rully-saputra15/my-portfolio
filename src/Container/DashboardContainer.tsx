b6bc997af485fbe494585f7a63bf96827ae998e0
rully saputra <rully.saputra4@gmail.com>
2021-08-04 19:51:06 +0700

    add alert when website opened in mobile browser

--- src/Container/DashboardContainer.tsx
+++ src/Container/DashboardContainer.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardPage from "../Component/DashboardPage";
 
 
 const DashboardContainer = () => {
+  const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    }
+  }
+  const [windowsDimensions, setWindowsDimensions] = useState(getWindowDimensions());
+  const [isAlerted, setIsAlerted] = useState(false);
+  useEffect(() => {
+    if (windowsDimensions.width >= 480 && windowsDimensions.width <= 720 && !isAlerted) {
+      alert('For your best experience\nPlease open the website from desktop\nThe mobile version is under development\nSorry!')
+      setIsAlerted(true)
+    }
+    setWindowsDimensions(getWindowDimensions());
+  }, [windowsDimensions, isAlerted])
   const goToSelectedSocialMedia = (link: string) => {
-    window.open(link,"_blank");
+    window.open(link, "_blank");
   }
 
   return (
